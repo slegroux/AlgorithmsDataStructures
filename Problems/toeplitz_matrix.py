b9bd012623b4a16c8e69606a3ab957359f615609
Sylvain Le Groux <sylvain.legroux@fulbrightmail.org>
2017-05-08 16:10:26 -0700

    toeplitz matrix

---
+++ Problems/toeplitz_matrix.py
@@ -0,0 +1,15 @@
+# input is a square matrix
+def is_toeplitz(mat):
+	diag = mat[0][0]
+	for i in range(len(mat)):
+		if mat[i][i] != diag:
+			return False
+	return True
+
+
+m = [[4, 4, 5, 6],
+	[2, 4, 4, 5],
+	[1, 2, 4, 4],
+	[0, 1, 2, 4]]
+
+print is_toeplitz(m)
