b03c976be5cf9b4b39df4d306c41a19b7036c0cf
Sylvain Le Groux <sylvain.legroux@fulbrightmail.org>
2017-06-08 13:48:30 -0700

    kth to last stick

---
+++ Problems/25_kth_to_last_node_linked_list.py
@@ -0,0 +1,64 @@
+class Node:
+	def __init__(self, value=None):
+		self.value = value
+		self.next = None
+
+
+class LinkedList():
+	def __init__(self, head=None):
+		head = Node(head)
+		self.head = head
+		if head:
+			self.length = 1
+		else:
+			self.length = 0
+
+	def append(self, value):
+		new = Node(value)
+		current = self.head
+		while current.next is not None:
+			current = current.next
+		current.next = new
+		self.length += 1
+
+	def print_list(self):
+		print "length: ", self.length
+		current = self.head
+		while current!=None:
+			print current.value
+			current = current.next
+
+	def kth_to_last(self, k):
+		# somewhat inefficient
+		current = self.head
+		counter = 0
+		if k > self.length:
+			return
+
+		if current is not None:
+			while counter < self.length - k:
+				current = current.next
+				counter += 1
+		return current.value
+
+	def kth_to_last_(self, k):
+		# k length "stick" trick
+		pt_l = self.head
+		pt_r = self.head
+		counter = 0
+		while counter < self.length - k:
+			counter += 1
+			pt_r = pt_r.next
+			pt_l = pt_l.next
+		return pt_l.value
+
+
+ll = LinkedList('a')
+ll.append('b')
+ll.append('c')
+ll.append('d')
+ll.print_list()
+print ll.kth_to_last(3)
+print ll.kth_to_last(1)
+print ll.kth_to_last_(3)
+print ll.kth_to_last_(1)
