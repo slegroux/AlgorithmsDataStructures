25aa275b838f46b568026a88c6d07e6bbfd125e1
Sylvain Le Groux <sylvain.legroux@fulbrightmail.org>
2017-06-11 14:28:31 -0700

    flatten recursively

---
+++ Problems/flatten_nested_list.py
@@ -0,0 +1,17 @@
+# flatten([1, [2, 3, [4]], 5, [[6]]]) => [1, 2, 3, 4, 5, 6]
+
+def flatten(arr):
+	global res
+	res = []
+	def traverse(arr):
+		if type(arr) == int:
+			res.append(arr)
+			return
+		for el in arr:
+			traverse(el)
+
+	traverse(arr)
+	return res
+
+
+print flatten([1, [2, 3, [4]], 5, [[6]]])
