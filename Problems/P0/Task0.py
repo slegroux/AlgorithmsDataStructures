956a7342ae62ea3a1e125f6b068fddc51a9faf05
Sylvain Le Groux <slegroux@ccrma.stanford.edu>
2021-01-01 21:49:05 -0800

    separate analysis from code

--- Problems/P0/Task0.py
+++ Problems/P0/Task0.py
@@ -5,15 +5,12 @@ It's ok if you don't understand how to read files.
 import csv
 
 with open('texts.csv', 'r') as f:
-    # time complexity (T) : O(n) (n = number of lines in text document)
-    # space complexity (S): O(n) (list of n lines of fixed length)
     reader = csv.reader(f)
     texts = list(reader)
 
 with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
-
 
 """
 TASK 0:
@@ -22,8 +19,7 @@ Print messages:
 "First record of texts, <incoming number> texts <answering number> at time <time>"
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
-# T: O(1) accessing an element in python list isin constant time (!= linked list)
-# S: O(1) constant memory
+
 print("First record of texts, {} texts {} at time {}".format(texts[0][0], texts[0][1], texts[0][2]))
 last = calls[-1]
 print("Last record of calls, {} calls {} at time {}, lasting {} seconds".format(last[0], last[1], last[2], last[3]))
